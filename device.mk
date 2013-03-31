d03d60940e31d71206abdc02510fafed49813efa
anders3408 <anders3408@yahoo.dk>
2013-04-01 02:51:36 +0300

    added qcom

--- device.mk
+++ device.mk
@@ -21,6 +21,7 @@ $(call inherit-product, device/htc/runnymede/media_a1026.mk)
 $(call inherit-product, device/htc/runnymede/media_htcaudio.mk)
 
 # Inherit qcom proprietary blobs
+$(call inherit-product, vendor/qcom/proprietary/qcom-vendor.mk)
 DEVICE_PACKAGE_OVERLAYS += vendor/aokp/overlay/runnymede
 DEVICE_PACKAGE_OVERLAYS += device/htc/runnymede/overlay
 
